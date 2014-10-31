7b58193920dd7bed0f0a03ea31289bacf259e610
yoav-zibin <yoav.zibin@gmail.com>
2014-10-31 15:09:59 -0400

    EMPTY

---
+++ tictactoe/game.js
@@ -0,0 +1,71 @@
+'use strict';
+
+angular.module('myApp')
+  .controller('Ctrl', function (
+      $window, $scope, $log, $timeout,
+      gameService, scaleBodyService, gameLogic) {
+
+    var moveAudio = new Audio('tictactoe/audio/move.wav');
+    moveAudio.load();
+
+    function sendComputerMove() {
+      gameService.makeMove(
+          gameLogic.createComputerMove($scope.board, $scope.turnIndex));
+    }
+
+    function updateUI(params) {
+      $scope.board = params.stateAfterMove.board;
+      $scope.delta = params.stateAfterMove.delta;
+      if ($scope.board === undefined) {
+        $scope.board = gameLogic.getInitialBoard();
+      } else {
+        // Only play a sound if there was a move (i.e., state is not empty).
+        moveAudio.play();
+      }
+      $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
+        params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
+      $scope.turnIndex = params.turnIndexAfterMove;
+
+      // Is it the computer's turn?
+      if ($scope.isYourTurn
+          && params.playersInfo[params.yourPlayerIndex].playerId === '') {
+        $scope.isYourTurn = false;
+        // Wait 500 milliseconds until animation ends.
+        $timeout(sendComputerMove, 500);
+      }
+    }
+
+    // Before getting any updateUI, we show an empty board to a viewer (so you can't perform moves).
+    updateUI({stateAfterMove: {}, turnIndexAfterMove: 0, yourPlayerIndex: -2});
+
+    $scope.cellClicked = function (row, col) {
+      $log.info(["Clicked on cell:", row, col]);
+      if (!$scope.isYourTurn) {
+        return;
+      }
+      try {
+        var move = gameLogic.createMove($scope.board, row, col, $scope.turnIndex);
+        $scope.isYourTurn = false; // to prevent making another move
+        gameService.makeMove(move);
+      } catch (e) {
+        $log.info(["Cell is already full in position:", row, col]);
+        return;
+      }
+    };
+    $scope.shouldSlowlyAppear = function (row, col) {
+      return $scope.delta !== undefined
+          && $scope.delta.row === row && $scope.delta.col === col;
+    }
+
+    scaleBodyService.scaleBody({width: 152, height: 152});
+
+    gameService.setGame({
+      gameDeveloperEmail: "yoav.zibin@gmail.com",
+      minNumberOfPlayers: 2,
+      maxNumberOfPlayers: 2,
+      exampleGame: gameLogic.getExampleGame(),
+      riddles: gameLogic.getRiddles(),
+      isMoveOk: gameLogic.isMoveOk,
+      updateUI: updateUI
+    });
+  });
