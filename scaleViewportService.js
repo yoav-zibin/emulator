c5dc72e4ef337166a4d3f169a1b0a012af3d69d6
yoav-zibin <yoav.zibin@gmail.com>
2014-12-22 11:20:50 -0500

    EMPTY

---
+++ scaleViewportService.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('myApp')
+  .service('scaleViewportService', function($window, $log) {
+    var doc = $window.document;
+    var gameSize = null;
+    var oldSizes = null;
+
+    function scaleBody(_gameSize) {
+      gameSize = _gameSize;
+      rescale();
+    }
+    function round2(num) {
+      return Math.round(num * 100) / 100;
+    }
+
+    function rescale() {
+      var body = doc.body;
+      if (gameSize === null) {
+        return;
+      }
+      var myGameWidth = gameSize.width;
+      var myGameHeight = gameSize.height;
+      var windowWidth = screen.width; // window.innerWidth; // body.clientWidth
+      var windowHeight = screen.height; // window.innerHeight; // I saw cases where body.clientHeight was 0.
+      if (oldSizes !== null) {
+        if (oldSizes.myGameWidth === myGameWidth &&
+            oldSizes.myGameHeight === myGameHeight &&
+            oldSizes.windowWidth === windowWidth &&
+            oldSizes.windowHeight === windowHeight) {
+          return; // nothing changed, so no need to change the transformations.
+        }
+      }
+      oldSizes = {
+          myGameWidth: myGameWidth,
+          myGameHeight: myGameHeight,
+          windowWidth: windowWidth,
+          windowHeight: windowHeight
+      };
+
+      var scaleX = windowWidth / myGameWidth;
+      var scaleY = windowHeight / myGameHeight;
+      var scale = round2(Math.min(scaleX, scaleY));
+      var content = 'width=' + myGameWidth + ', height=' + myGameHeight + ', initial-scale=' + scale + ', minimum-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=no';
+      var viewports = document.getElementsByName("viewport");
+      var hasViewport = viewports && viewports.length > 0;
+      $log.info(["Scaling the body to size: ", oldSizes, ' scale:', scale, 'viewport content:', content, 'hasViewport:', hasViewport]);
+      if (hasViewport) {
+        viewports[0].setAttribute("content", content);
+      }
+    }
+
+    $window.onresize = rescale;
+    $window.onorientationchange = rescale;
+    doc.addEventListener("onresize", rescale);
+    doc.addEventListener("orientationchange", rescale);
+    setInterval(rescale, 1000);
+
+    this.scaleBody = scaleBody;
+  });
