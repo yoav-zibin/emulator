9c908590271cc940186c22fa0a67a2edb5a5f930
yoav-zibin <yoav.zibin@gmail.com>
2015-03-10 13:16:32 -0400

    EMPTY

---
+++ angular-translate/languages/de.js
@@ -0,0 +1,7 @@
+window.angularTranslations = {
+  HEADLINE: "Hey, das ist meine großartige App!",
+  INTRO_TEXT: "Und sie untersützt mehrere Sprachen!",
+  WITH_VALUES: "Es ist dynamisch: {{value}}",
+  BUTTON_TEXT_EN: "englisch",
+  BUTTON_TEXT_DE: "deutsch"
+};
---
+++ angular-translate/languages/en.js
@@ -0,0 +1,7 @@
+window.angularTranslations = {
+  HEADLINE: "Hello there, This is my awesome app!",
+  INTRO_TEXT: "And it has i18n support!",
+  WITH_VALUES: "The following value is dynamic: {{value}}",
+  BUTTON_TEXT_EN: "english",
+  BUTTON_TEXT_DE: "german"
+};
