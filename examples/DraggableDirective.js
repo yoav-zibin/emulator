d68156152fa57d8924d7f4a8bb3e9f5dc4c517f9
yoav_zibin <yoav.zibin@gmail.com>
2015-03-15 15:57:04 -0400

    Drag-n-drop snap example

--- examples/DraggableDirective.js
+++ examples/DraggableDirective.js
@@ -10,11 +10,20 @@
       link: function (scope, element, attrs) {
         if (attrs['ddDraggable'] === 'true') {
           element.draggable({
+            snap: ".snapTarget",
+            snapTolerance: element.width() / 2,
+            snapMode: "inner",
             revert: "invalid",
             start: function () {
+              // Setting snapTolerance again because the width might change due to orientation change.
+              element.draggable( "option", "snapTolerance", element.width() / 2);
               var id = element[0].id;
               scope.handleDragStart(id);
-            }
+            }/*,
+            drag: function( event, ui ) {
+              //console.log(ui.position.left + "x" + ui.position.top + " offset: " + ui.offset.left + "x" + ui.offset.top);
+              //ui.position.left = Math.min( 100, ui.position.left );
+            }*/
           });
         }
       }
