4ad54ae8bc739d4156306dfb79bdaf28e958b85e
yoav_zibin <yoav.zibin@gmail.com>
2015-04-25 12:06:31 -0400

    angularTranslationsLoaded

--- angular-translate/languages/de.js
+++ angular-translate/languages/de.js
@@ -1,7 +1,7 @@
-window.angularTranslations = {
+window.angularTranslationsLoaded('de', {
   HEADLINE: "Hey, das ist meine großartige App!",
   INTRO_TEXT: "Und sie untersützt mehrere Sprachen!",
   WITH_VALUES: "Es ist dynamisch: {{value}}",
   BUTTON_TEXT_EN: "englisch",
   BUTTON_TEXT_DE: "deutsch"
-};
+});
