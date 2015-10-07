e1fad33dfad849d33e11aef2370bf293cedc709e
yoav-zibin <yoav.zibin@gmail.com>
2015-10-07 13:55:39 -0400

    EMPTY

--- dist/turnBasedServices.3.js
+++ dist/turnBasedServices.3.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-"use strict"; var emulatorServicesCompilationDate = "Wed Oct 7 13:55:06 EDT 2015";
-=======
-"use strict"; var emulatorServicesCompilationDate = "Tue Sep 22 23:42:29 CEST 2015";
->>>>>>> 382cc6d42fb26fc4c84af5df3b2ca4fac926611e
+"use strict"; var emulatorServicesCompilationDate = "Wed Oct 7 13:55:37 EDT 2015";
 ;var log;
 (function (log_1) {
     var ILogLevel = (function () {
