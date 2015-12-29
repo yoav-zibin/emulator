3f7da4e028e2e99beaf20fd80291ed8c49afc4a3
yoav_zibin <yoav.zibin@gmail.com>
2015-12-29 07:41:32 -0500

    x

--- ts_output_readonly_do_NOT_change_manually/src/copyNamespaceToWindow.js
+++
@@ -1,9 +0,0 @@
-// Copy everything on gamingPlatform to window,
-// for backward compatability with games that don't use the gamingPlatform namespace.
-(function () {
-    var w = window;
-    var g = gamingPlatform;
-    for (var key in g) {
-        w[key] = g[key];
-    }
-})();
