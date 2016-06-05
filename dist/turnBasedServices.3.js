62c305631aa6db37dfbf487523e32f56d2b90a37
yoav_zibin <yoav.zibin@gmail.com>
2016-06-05 11:18:14 -0400

    x

--- dist/turnBasedServices.3.js
+++ dist/turnBasedServices.3.js
@@ -1,4 +1,4 @@
-"use strict"; var emulatorServicesCompilationDate = "Sun Jun 5 11:11:18 EDT 2016";
+"use strict"; var emulatorServicesCompilationDate = "Sun Jun 5 11:18:11 EDT 2016";
 
 ;
 var gamingPlatform;
