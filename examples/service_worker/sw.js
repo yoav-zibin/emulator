53636cf8fce98abe2a1cc8cd7c67ba4abfd91a9b
yoav-zibin <yoav.zibin@gmail.com>
2016-06-06 16:02:22 -0400

    x

---
+++ examples/service_worker/sw.js
@@ -0,0 +1,29 @@
+// The files we want to cache
+var urlsToCache = [
+  'index.html',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        console.log('Fetch response=', response);
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
